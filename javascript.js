71a09aedd5998054c509bc1af8eb82d230eb8c2f
maheshb02 <bedademahesh02@gmail.com>
2022-08-31 10:56:32 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// console.log(document);
+// console.log(document.URL);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.forms);
+
+
+// let header = document.getElementById("main-header");
+// header.style.borderBottom ="solid 5px #908"
+
+let title = document.getElementsByClassName("title")[0]
+title.textContent="gjjv"
