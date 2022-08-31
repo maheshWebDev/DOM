26fd5d06cfa4e69592e2a5bf5365769268b92a3f
maheshb02 <bedademahesh02@gmail.com>
2022-08-31 17:45:01 +0530

    task7 manipulate the DOM

--- javascript.js
+++ javascript.js
@@ -20,15 +20,28 @@
 // console.log(list)
 // list[4].style.fontWeight="bold";
 
-let secondItem = document.querySelector(".list-group-item:nth-child(2)");
-secondItem.style.backgroundColor="green"
-let thardItem = document.querySelector(".list-group-item:nth-child(3)");
-thardItem.style.color="red";
+// let secondItem = document.querySelector(".list-group-item:nth-child(2)");
+// secondItem.style.backgroundColor="green"
+// let thardItem = document.querySelector(".list-group-item:nth-child(3)");
+// thardItem.style.color="red";
 
-let second = document.querySelectorAll(".list-group-item");
-second[1].style.color="white";
+// let second = document.querySelectorAll(".list-group-item");
+// second[1].style.color="white";
 
-let odd = document.querySelectorAll("li:nth-child(odd)")
-for(let i =0; i<odd.length;i++){
-    odd[i].style.backgroundColor="red";
-}
+// let odd = document.querySelectorAll("li:nth-child(odd)")
+// for(let i =0; i<odd.length;i++){
+//     odd[i].style.backgroundColor="red";
+// }
+
+
+// add HEllo word before Item Lister
+
+let newDiv = document.createElement("div");
+newDiv.className = "hello";
+newDiv.id="hello1";
+newDiv.setAttribute('title','hello div');
+let newDivText = document.createTextNode("hello world");
+newDiv.appendChild(newDivText);
+let container = document.querySelector('header .container');
+let h1= document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
