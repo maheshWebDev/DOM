bfa7511447ee45a77273b29be2076637cdbca2c3
maheshb02 <bedademahesh02@gmail.com>
2022-08-31 15:48:41 +0530

    task5 tagname

--- javascript.js
+++ javascript.js
@@ -9,9 +9,13 @@
 // let header = document.getElementById("main-header");
 // header.style.borderBottom ="solid 5px #008"
 //  getElementsByClassName
-let items = document.getElementsByClassName("list-group-item");
-console.log(items)
-items[2].style.backgroundColor="green"
-for(let i = 0;i<items.length;i++){
-    items[i].style.fontweight="bold";
-}
+// let items = document.getElementsByClassName("list-group-item");
+// console.log(items)
+// items[2].style.backgroundColor="green"
+// for(let i = 0;i<items.length;i++){
+//     items[i].style.fontweight="bold";
+// }
+
+let list = document.getElementsByTagName("li");
+console.log(list)
+list[4].style.fontWeight="bold";
