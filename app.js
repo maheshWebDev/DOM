a2d1a335aa2fedfa7fab84868d1c356e7f6ac5da
maheshb02 <bedademahesh02@gmail.com>
2022-09-05 18:05:49 +0530

    task 11

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+// let form = document.getElementById("myform");
+// form.addEventListener("submit", addToLocal);
+// function addToLocal(e){
+//     e.preventDefault();
+//     let firstName= document.getElementById("name").value 
+//     let email = document.getElementById("email").value
+//     localStorage.setItem("name", firstName);
+//     localStorage.setItem("email",email);
+// }
+
 let form = document.getElementById("myform");
 form.addEventListener("submit", addToLocal);
 function addToLocal(e){
     e.preventDefault();
     let firstName= document.getElementById("name").value 
     let email = document.getElementById("email").value
-    localStorage.setItem("name", firstName);
-    localStorage.setItem("email",email);
+ const myObj = {
+    name : firstName,
+    email : email
+ }
+ let myObjDeserialized = JSON.stringify(myObj);
+ localStorage.setItem("userDetails",myObjDeserialized);
 }
